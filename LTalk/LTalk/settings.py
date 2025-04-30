d77e335b34cd711b5f6d8b8211a886d1e15eba80
Anna Nevidoma <an2015te@gmail.com>
2025-04-30 18:11:54 +0300

    Update template directory path in settings.py to point to main/templates/main

--- LTalk/LTalk/settings.py
+++ LTalk/LTalk/settings.py
@@ -57,7 +57,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            BASE_DIR / 'templates'
+            BASE_DIR / 'main/templates/main'
         ],
         'APP_DIRS': True,
         'OPTIONS': {
