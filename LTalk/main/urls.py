c9867dad70214b8b66cfca14aff5639db8cbce80
Kirill Postrehan <themrshayzoom882@gmail.com>
2025-05-01 20:17:45 +0300

    Add basic wordset detail page

--- LTalk/main/urls.py
+++ LTalk/main/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import home, create_set
+from .views import home, create_set, wordset_detail
 
 urlpatterns = [
     path('', home, name='home'),
     path('create-set', create_set, name='create_set'),
+    path('wordsets/<int:id>/', wordset_detail, name='wordset_detail'),
 ]
--- LTalk/main/views.py
+++ LTalk/main/views.py
@@ -1,35 +1,10 @@
-from django.shortcuts import render, redirect
-from django.contrib import messages
-from django.http import JsonResponse, HttpResponseBadRequest
-from django.views.decorators.csrf import csrf_exempt
-from django.views.decorators.http import require_http_methods
+from django.shortcuts import get_object_or_404, render, redirect
 from django.contrib.auth.decorators import login_required
 
 from .models import Word, WordSet
 
-import google.generativeai as genai
-import PIL.Image
-from dotenv import load_dotenv
-import os
-import json
 
-# Load environment variables from .env file
-load_dotenv()
 
-# Access the API key
-api_key = os.getenv('GOOGLE_API_KEY')
-if not api_key:
-    raise Exception("Please set the GOOGLE_API_KEY in your .env file.")
-
-# Configure the API key
-genai.configure(api_key=api_key)
-
-# ...existing code...
-
-prompt_text = ("Look at the image, extract only lithuanian words and give me their translation. "
-               "Write original Lithuanian words in infinitive form. Format the response as a JSON array "
-               "with objects containing 'word', 'translation', and 'infinitive' fields. "
-               "Example format: [{\"word\":\"word\",\"translation\":\"translation\",\"infinitive\":\"infinitive\"}]")
 
 @login_required(login_url='login')
 def home(request):
@@ -41,3 +16,9 @@ def create_set(request):
     if request.method == 'POST':
             return redirect('home')
     return render(request, "create_set.html")
+
+
+@login_required(login_url='login')
+def wordset_detail(request, id):
+    wordset = get_object_or_404(WordSet, pk=id)
+    return render(request, 'wordset_detail.html', {'wordset': wordset})
