12663e8b2e031a65cdf2181f84730ace175a3894
Anna Nevidoma <an2015te@gmail.com>
2025-05-01 19:38:51 +0300

    Add flashcard functionality with styles and JavaScript; implement flashcard practice view and update templates for user interaction.

--- LTalk/main/urls.py
+++ LTalk/main/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import home, create_set
+from .views import home, create_set, flashcard_practice
 
 urlpatterns = [
     path('', home, name='home'),
     path('create-set', create_set, name='create_set'),
+    path('wordset/<int:wordset_id>/flashcard/', flashcard_practice, name='flashcard_practice'),
 ]
